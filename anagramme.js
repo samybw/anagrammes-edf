bd41ff04a2f246808780b63dd3bb9ee97a141925
samy <samybn@yahoo.com>
2020-05-29 17:10:10 +0200

    Modification de mon if et j'ai Reussi !

---
+++ anagramme.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+const arg = process.argv.slice(2);
+const argument = arg.map((element) => { 
+    var newElement = element.split('').sort().join('') ;
+    return newElement;
+})
+
+
+const data = fs.readFileSync('dictionnaire.txt', 'UTF-8');
+const tableaufile = data.split(/\r?\n/).map((element) => { 
+    var newElement = element ;
+    return newElement;
+})
+
+
+var i = 0 ;
+
+while (i < tableaufile.length -1) {
+    
+        if(tableaufile[i].split('').sort().join('') == argument){
+        console.log(tableaufile[i]);
+        
+        } 
+        i++
+}
+
+
+
+
+
